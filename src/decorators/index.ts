f8df0479c112d8d4a829ff848fb850a1672741f5
Edoardo Luppi <lp.edoardo@gmail.com>
2025-06-22 12:47:27 +0200

    refactor: clean up index exports

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,4 +1,5 @@
 export { AutoRegister } from "./autoRegister";
+export type { ClassDecorator, ClassFieldDecorator } from "./decorators";
 export { Inject } from "./inject";
 export { Injectable } from "./injectable";
 export { InjectAll } from "./injectAll";
