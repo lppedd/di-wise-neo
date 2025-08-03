3fab99f666db9b3e99173cee1cc8f81ea3858970
Edoardo Luppi <lp.edoardo@gmail.com>
2025-08-03 17:36:36 +0200

    test: verify the string representation for all token types

--- src/__tests__/token.spec.ts
+++ src/__tests__/token.spec.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from "vitest";
 
 import { createType } from "..";
+import { getTypeName } from "../utils/typeName";
 
 describe("Type", () => {
   it("should create a unique token", () => {
@@ -10,5 +11,16 @@ describe("Type", () => {
   it("should have toString", () => {
     // eslint-disable-next-line @typescript-eslint/no-base-to-string
     expect(String(createType("foo"))).toBe("Type<foo>");
+  });
+
+  it("should have the correct string representation", () => {
+    expect(getTypeName(null)).toBe("null");
+    expect(getTypeName(undefined)).toBe("undefined");
+    expect(getTypeName("myString")).toBe('"myString"');
+    expect(getTypeName(function MyFunction() {})).toBe("typeof MyFunction");
+    expect(getTypeName(() => {})).toBe("Function");
+
+    class MyClass {}
+    expect(getTypeName(new MyClass())).toBe("MyClass");
   });
 });
