2ffda84594a7baf41cf1475da99c59b3925b386e
Edoardo Luppi <lp.edoardo@gmail.com>
2025-10-11 21:05:07 +0200

    chore: add a JSDoc to each scope

--- src/scope.ts
+++ src/scope.ts
@@ -1,6 +1,22 @@
 export const Scope = {
+  /**
+   * Creates a new value every time the token is resolved.
+   */
   Transient: "Transient",
+
+  /**
+   * Creates and caches a single value per token resolution graph.
+   *
+   * The same value is reused during a single resolution request and is subsequently discarded.
+   */
   Resolution: "Resolution",
+
+  /**
+   * Creates and caches a single value per container.
+   *
+   * If the value is not found in the current container, it is looked up in the parent container,
+   * and so on. It effectively behaves like a _singleton_ scope but allows container-specific overrides.
+   */
   Container: "Container",
 } as const;
 
