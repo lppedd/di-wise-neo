lppedd/di-wise-neo

commit da61bd2aee53e98b8eab7dbeadc31d7dade6962f

refactor!: remove Type.inter and Type.union
Their value was definitely low compared to the cost
of having to deal with them long term.

src/token.ts
@@ -1,37 +1,20 @@
 /**
  * Type API.
  */
-export interface Type<A> {
+export interface Type<T> {
   /**
    * The name of the type.
    */
   readonly name: string;
 
   /**
-   * Creates an intersection type from another type.
+   * Ensures that different `Type<T>` types are not structurally compatible.
    *
-   * @example
-   * ```ts
-   * const A = createType<A>("A");
-   * const B = createType<B>("B");
+   * This property is never used at runtime.
    *
-   * A.inter("I", B); // => Type<A & B>
-   * ```
+   * @private
    */
-  inter<B>(typeName: string, B: Type<B>): Type<A & B>;
-
-  /**
-   * Creates a union type from another type.
-   *
-   * @example
-   * ```ts
-   * const A = createType<A>("A");
-   * const B = createType<B>("B");
-   *
-   * A.union("U", B); // => Type<A | B>
-   * ```
-   */
-  union<B>(typeName: string, B: Type<B>): Type<A | B>;
+  readonly __type?: T;
 }
 
 /**
@@ -65,16 +48,11 @@ export type Tokens<Value = any> = [Token<Value>, ...Token<Value>[]];
  * @__NO_SIDE_EFFECTS__
  */
 export function createType<T>(typeName: string): Type<T> {
-  const type = {
-    name: `Type<${typeName}>`,
-    inter: createType,
-    union: createType,
-    toString(): string {
-      return type.name;
-    },
+  const name = `Type<${typeName}>`;
+  return <Type<T>>{
+    name: name,
+    toString: () => name,
   };
-
-  return type;
 }
 
 // @internal

commit 0019bb66991aea7f22f8919960a829630860dc9e

fix: attempt to restore strict type checking for Type
This was broken in da61bd2aee53e98b8eab7dbeadc31d7dade6962f

src/containerImpl.ts
@@ -26,7 +26,7 @@ import {
   type Provider,
 } from "./provider";
 import { Scope } from "./scope";
-import { type Constructor, isConstructor, type ProviderType, type Token, type Type } from "./token";
+import { type Constructor, isConstructor, type ProviderType, type Token } from "./token";
 import { isBuilder, type MethodDependency, type Registration, type RegistrationOptions, TokenRegistry } from "./tokenRegistry";
 import { isDisposable } from "./utils/disposable";
 
@@ -140,10 +140,10 @@ export class ContainerImpl implements Container {
       if (isConstructor(token)) {
         this.registerClass(token);
       } else {
-        this.registerType(token, token.provider, token.options);
+        this.registerToken(token, token.provider, token.options);
       }
     } else {
-      this.registerType(...args);
+      this.registerToken(...args);
     }
 
     return this;
@@ -251,9 +251,9 @@ export class ContainerImpl implements Container {
     }
   }
 
-  private registerType<T>(type: Type<T>, provider: Provider<T>, options?: RegistrationOptions): void {
+  private registerToken<T>(token: Token<T>, provider: Provider<T>, options?: RegistrationOptions): void {
     const name = provider.name;
-    check(name === undefined || name.trim(), `name qualifier for token ${getTokenName(type)} must not be empty`);
+    check(name === undefined || name.trim(), `name qualifier for token ${getTokenName(token)} must not be empty`);
 
     if (isClassProvider(provider)) {
       const metadata = getMetadata(provider.useClass);
@@ -269,19 +269,19 @@ export class ContainerImpl implements Container {
         dependencies: metadata.dependencies,
       };
 
-      this.myTokenRegistry.set(type, registration);
+      this.myTokenRegistry.set(token, registration);
 
       // Eager-instantiate only if the provided class is container-scoped
       if (metadata.eagerInstantiate && registration.options?.scope === Scope.Container) {
-        this.resolveProviderValue(type, registration);
+        this.resolveProviderValue(token, registration);
       }
     } else {
       if (isExistingProvider(provider)) {
         const [targetToken] = this.getTargetToken(provider);
-        check(type !== targetToken, `token ${getTokenName(type)} cannot alias itself via useExisting`);
+        check(token !== targetToken, `token ${getTokenName(token)} cannot alias itself via useExisting`);
       }
 
-      this.myTokenRegistry.set(type, {
+      this.myTokenRegistry.set(token, {
         name: name,
         provider: provider,
         options: options,

src/token.ts
@@ -11,18 +11,13 @@ export interface Type<T> {
   readonly name: string;
 
   /**
-   * Returns the stringified representation of the type.
-   */
-  readonly toString: () => string;
-
-  /**
    * Ensures that different `Type<T>` types are not structurally compatible.
    *
-   * This property is never used at runtime.
+   * This property is always `undefined` and is never used at runtime.
    *
    * @private
    */
-  readonly __type?: T;
+  readonly __type: T | undefined;
 }
 
 /**
@@ -52,7 +47,7 @@ export interface Constructor<Instance extends object> {
  * Token type.
  */
 export type Token<Value = any> = [Value] extends [object] // Avoids distributive union behavior
-  ? Type<Value> | Constructor<Value>
+  ? Type<Value> | Constructor<Value & object>
   : Type<Value>;
 
 /**
@@ -98,8 +93,8 @@ export function createType<T>(
   const name = `Type<${typeName}>`;
   const toString = (): string => name;
   return provider //
-    ? { name, provider, options, toString }
-    : { name, toString };
+    ? ({ name, provider, options, toString } as ProviderType<T>)
+    : ({ name, toString } as Type<T>);
 }
 
 // @internal

src/__tests__/token.spec.ts
@@ -9,6 +9,7 @@ describe("Type", () => {
   });
 
   it("should have toString", () => {
+    // eslint-disable-next-line @typescript-eslint/no-base-to-string
     expect(String(createType("foo"))).toBe("Type<foo>");
   });
 