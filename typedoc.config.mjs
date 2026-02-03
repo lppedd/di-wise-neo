e17a74f75c4e478d235847ed3ba4c4a6d188e6b9
Edoardo Luppi <lp.edoardo@gmail.com>
2026-02-03 23:57:27 +0100

    chore: adjust Typedoc config

--- typedoc.config.mjs
+++ typedoc.config.mjs
@@ -1,6 +1,8 @@
 /** @type {Partial<import("typedoc").TypeDocOptions>} */
 const config = {
   intentionallyNotExported: [
+    "src/container.ts:ProviderFor",
+    "src/container.ts:RegistrationOptionsFor",
     "src/decorators/decorators.ts:ClassDecorator",
     "src/decorators/decorators.ts:ParameterDecorator",
     "src/utils/requiredNonNullable.ts:RequiredNonNullable",
